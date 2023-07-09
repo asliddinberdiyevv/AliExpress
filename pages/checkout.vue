07a5d05e8bce802514b3353adc7927017a9e2c4b
asliddinberdiyevv <asliddinberdiyevv@gmail.com>
2023-07-09 20:36:05 +0500

    checkout page

---
+++ pages/checkout.vue
@@ -0,0 +1,123 @@
+<script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+import { useUserStore } from "~/stores/user";
+const userStore = useUserStore();
+const route = useRoute();
+
+let stripe = null;
+let elements = null;
+let card = null;
+let form = null;
+let total = ref(0);
+let clientSecret = null;
+let currentAddress = ref(null);
+let isProcessing = ref(false);
+
+const products = [
+  {
+    id: 1,
+    title: "Title 1",
+    description: "This is a description",
+    url: "https://picsum.photos/id/7/800/800",
+    price: 9899,
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    description: "This is a description",
+    url: "https://picsum.photos/id/71/800/800",
+    price: 9699,
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    description: "This is a description",
+    url: "https://picsum.photos/id/72/800/800",
+    price: 9969,
+  },
+];
+
+watch(
+  () => total.value,
+  () => {
+    if (total.value > 0) {
+      stripeInit();
+    }
+  }
+);
+
+const stripeInit = async () => {};
+const pay = async () => {};
+const createOrder = async (stripeId) => {};
+const showError = async (errorMsgText) => {};
+
+onMounted(() => {
+  isProcessing.value = true;
+  userStore.checkout.forEach((item) => {
+    total.value += item.price;
+  });
+});
+</script>
+
+<template>
+  <MainLayout>
+    <div id="CheckoutPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+      <div class="md:flex gap-4 justify-between mx-auto w-full">
+        <div class="md:w-[65%]">
+          <div class="bg-white rounded-lg p-4">
+            <div class="text-xl font-semibold mb-2">Shipping Address</div>
+            <div v-if="true">
+              <NuxtLink
+                to="/address"
+                class="flex items-center pb-2 text-blue-500 hover:text-red-400"
+              >
+                <Icon name="mdi:plus" size="18" class="mr-2" />
+                Update Address
+              </NuxtLink>
+              <div class="pt-2 border-t">
+                <div class="underline pb-1">Delivery Address</div>
+                <ul class="text-xs">
+                  <li class="flex items-center gap-2">
+                    <div>Contact Name:</div>
+                    <div class="font-bold">TEST</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Address:</div>
+                    <div class="font-bold">TEST</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Zip Code:</div>
+                    <div class="font-bold">TEST</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>City:</div>
+                    <div class="font-bold">TEST</div>
+                  </li>
+                  <li class="flex items-center gap-2">
+                    <div>Country:</div>
+                    <div class="font-bold">TEST</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <NuxtLink
+              v-else
+              to="/address"
+              class="flex items-center text-blue-500 hover:text-red-400"
+            >
+              <Icon name="mdi:plus" size="18" class="mr-2" />
+              Add New Address
+            </NuxtLink>
+          </div>
+
+          <div id="Items" class="bg-white rounded-lg p-4 mt-4">
+            <div v-for="product in products">
+              <CheckoutItem :product="product" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </MainLayout>
+</template>
